573b9c985a2c752680450b4fd6c06b56efbb395b
Mohji12 <100218595+Mohji12@users.noreply.github.com>
2025-11-25 01:07:57 +0530

    Update API_BASE_URL for backend connectionl

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@
 // Force local backend for development
 //const API_BASE_URL = "http://127.0.0.1:8000";
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
-const API_BASE_URL = "https://6hgfjwmhx4.execute-api.ap-south-1.amazonaws.com";
+const API_BASE_URL = "https://nlq4zcho6j.execute-api.ap-south-1.amazonaws.com";
 
 // console.log('🚀 API_BASE_URL initialized to:', API_BASE_URL);
 
